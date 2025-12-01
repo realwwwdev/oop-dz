5a0352376d31ccdac906fd7f56aef93316875678
i.saetgareev <i.saetgareev@bftcom.com>
2025-12-01 17:16:45 +0300

    дополнен класс Dish (конструктор, гет/сет, метод toString)

--- src/main/java/model/Menus/Dish.java
+++ src/main/java/model/Menus/Dish.java
@@ -6,4 +6,24 @@ public class Dish {
     String name;
     double price;
     Category category;
+
+    public Dish(String name, double price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public Category getCategory() {
+        return category;
+    }
+
+    public String toString() {
+        return "Название: " + name + " Цена: " + price + " Категория: " + category;
+    }
+
 }
